443fbadc180e7ff847cc5b68b8dfd9c1ba3bf70a
Yeti-Spaghetti <yetchi_yeung@hotmail.com>
2020-06-22 13:43:22 +0930

    fc layer implemented
    - got fc layer working correctly
    - fixed activation layer, now works with each layers specified activation function for forward pass

--- cnn.py
+++ cnn.py
@@ -17,56 +17,54 @@ class NeuralNetwork:
         self.convolve = []
         self.convolve.append(img)
         self.convolve = np.array(self.convolve)
+        self.convActivation = []
+
         self.filterTuple = (1,3,3)
         self.filters = []
-        self.filterBias = []
         self.maxPoolCount = 0
-        self.lastLayer = None
-        self.weights1 = None
-        self.fcBias = 1
 
-    def Conv2D(self, layers=32, convSize=3):
+        # self.lastLayer = None
+        # self.weights1 = None
+
+        self.fcLayersSizes = []
+        self.fcLayers = []
+        self.fcLayersActivations = []
+
+    def Conv2D(self, filters=32, convSize=3, activation='relu'):
         self.maxPoolCount += 1
-        self.ConvFilters(layers, convSize)
+        self.convActivation.append(activation)
+        self.ConvFilters(filters, convSize)
 
-    def ConvFilters(self, layers=32, size=3):
-        self.lastLayer = layers
+    def ConvFilters(self, filters=32, size=3):
+        self.lastLayer = filters
         self.filterTuple = list(self.filterTuple)
-        self.filterTuple[0] = layers
+        self.filterTuple[0] = filters
         self.filterTuple = tuple(self.filterTuple)
 
-        self.filters.append((np.random.random_sample(self.filterTuple)))
+        self.filters.append(np.random.uniform(-0.1,0.1,self.filterTuple))
 
-    def activation_layer(self, activation='ReLu', derivative=False):
+    def activation(self, activation='ReLu', derivative=False):
+        activation = activation.lower()
         for i in range(len(self.convolve)):
-            if activation == 'ReLu':
+            if activation == 'relu':
                 if derivative:
                     self.convolve[i] = 1*(self.convolve[i]>0)
                 else:
                     self.convolve[i] = self.convolve[i]*(self.convolve[i]>0)
-            if activation == 'Sigmoid':
+
+            if activation == 'sigmoid':
                 if derivative:
                     self.convolve[i] = 1/(1+np.ex(-self.convolve[i])) * (1-(1/(1+np.ex(-self.convolve[i]))))
                 else:
                    self.convolve[i] = 1/(1+np.ex(-self.convolve[i]))
-            if activation == 'LeakyReLu':
-                pass
-            
-    def pad_columns(self, filtSize, img):
-        # pad columns of zeros
-        r = filtSize//2
-        col = np.zeros((self.img.shape[0], r))
-        paddedImg = np.hstack((self.img,col))
-        paddedImg = np.hstack((col,paddedImg))  
-        return paddedImg
 
-    def pad_rows(self, filtSize, img):
-        # pad row of zeros
-        c = filtSize//2
-        row = np.zeros((c, img.shape[1]))
-        paddedImg = np.vstack((img, row))
-        paddedImg = np.vstack((row, paddedImg))
-        return paddedImg
+            if activation == 'leakyreLu':
+                self.convolve[i] = np.where(self.convolve > 0, self.convolve, self.convolve * 0.01)
+
+            if activation == 'softmax':
+                expSum = np.sum(np.exp(self.convolve))
+                for i in range(len(self.convolve)):
+                    self.convolve[i] = np.exp(self.convolve[i])/expSum
 
     def MaxPool_padding(self, input_, poolSize, stride):
         if input_.ndim == 2:
@@ -85,7 +83,7 @@ class NeuralNetwork:
                 input_ = temp
 
             while (input_.shape[-2] - poolSize) % stride != 0:
-                input_T = input_.transpose
+                input_T = np.transpose(input_)
                 y,x = input_T.shape
                 #create temporary array
                 temp = np.zeros((y, x+1))
@@ -104,6 +102,9 @@ class NeuralNetwork:
             retArr.append(self.MaxPool_padding(input_[i], poolSize, stride))
         retArr = np.array(retArr)
         return retArr
+    
+    def MaxPool_padding_helper(self, input_, poolSize, stride):
+        pass
 
     # find maximum value in sliding window and return array
     def MaxPooling(self, input_, poolSize, stride):
@@ -142,14 +143,33 @@ class NeuralNetwork:
         # # find maximum value in sliding window and create new array
         self.convolve = self.MaxPooling(input_, poolSize, stride)
 
+    def fcLayer(self, size=4096, activation='relu'):
+        self.fcLayersSizes.append(size)
+        self.fcLayersActivations.append(activation)
+
+    def fcLayersCreate(self):
+        for i in range(len(self.fcLayersSizes)-1):
+            fcLayer = np.random.uniform(-0.1,0.1,(self.fcLayersSizes[i], self.fcLayersSizes[i+1]))
+            self.fcLayers.append(fcLayer)
+        self.fcLayers = np.array(self.fcLayers)
+ 
+    def fcLayersTrain(self):
+        for i in range(len(self.fcLayers)):           
+            self.convolve = self.convolve.dot(self.fcLayers[i])
+            self.activation(self.fcLayersActivations[i])
+            
     def TrainCNN(self, epochs):
+        # necessary conversion for numpy operations
         self.filters = np.array(self.filters)
-        self.weights1 = np.random.rand(self.lastLayer * int(self.convolve.shape[1] * self.convolve.shape[2] / 4**self.maxPoolCount))
+        # calculate size of flattened convolutional layer
+        self.fcLayersSizes.insert(0,self.lastLayer * math.ceil(self.convolve.shape[1]/2**self.maxPoolCount) * math.ceil(self.convolve.shape[2]/2**self.maxPoolCount))
+        # initialize fc layers
+        self.fcLayersCreate()
 
         for e in range(epochs):
             # FORWARD PASS
             # convolution layer
-            for i in range(len(self.filters)):
+            for i in range(0,len(self.filters)):
                 temp = []
                 for j in range(len(self.filters[i])):
                     convolveSum = np.zeros((self.convolve.shape[1], self.convolve.shape[2]))
@@ -159,17 +179,26 @@ class NeuralNetwork:
                 self.convolve = np.array(temp)
 
                 # activation function layer
-                self.activation_layer()
+                self.activation(self.convActivation[i])
 
                 # max-pool layer
                 self.MaxPool()
 
-            # fc layer
-            fc = self.convolve.flatten()
-            predict = np.dot(fc,self.weights1) + self.fcBias
-            print(predict)
+            # flatten conv2d output
+            self.convolve = self.convolve.flatten()
+            self.convolve = self.convolve.reshape((1,self.convolve.shape[0]))
+            # fully-connected layer step
+            self.fcLayersTrain()
+
+            # BACKPROPAGATION
+
 
 nn = NeuralNetwork(x_train[0])
-nn.Conv2D(10)
-nn.Conv2D(20)
-nn.TrainCNN(1)
+nn.Conv2D(10, 'relu')
+nn.Conv2D(20, 'relu')
+nn.Conv2D(30, 'relu')
+nn.fcLayer(4096, 'relu')
+nn.fcLayer(4096, 'relu')
+nn.fcLayer(10, 'SoftMax')
+
+nn.TrainCNN(epochs=1)
